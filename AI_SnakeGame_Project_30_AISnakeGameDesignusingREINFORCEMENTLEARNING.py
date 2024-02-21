0fa29f00212a141acd751c4306703acf7b7ee0e8
Sujalshah46 <sujalkiranshah@gmail.com>
2024-02-21 19:10:32 +0530

    Initial commit of project files

---
+++ AI_SnakeGame_Project_30_AISnakeGameDesignusingREINFORCEMENTLEARNING.py
@@ -0,0 +1,15 @@
+#Project_30_AISnakeGameDesignusingREINFORCEMENTLEARNING_BY_EdgeAIHub
+from snake.game import Game, GameConf, GameMode
+
+greedy = "GreedySolver"
+hamilton = "HamiltonSolver"
+
+normal = GameMode.NORMAL
+
+
+conf = GameConf()
+conf.solver_name = hamilton
+conf.mode = normal
+print("Solver: %s " % (conf.solver_name))
+print("Mode: %s" %(conf.mode))
+Game(conf).run()
